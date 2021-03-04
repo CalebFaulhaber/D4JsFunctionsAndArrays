1d2ed65b178088df2fb9c6601c60c445e9947be1
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-04 11:45:06 -0800

    Created 5th lab file

---
+++ L5IsHomogenous.js
@@ -0,0 +1,15 @@
+const isHomogenous = (arr) => {
+    
+}
+
+// Is Homogenous?
+
+// Write a function, isHomogenous, that takes an array and returns true only if all its values are of the same type (e.g. all the values are strings or all the values are numbers, etc).
+
+// Example:
+
+// isHomogenous([1, 2, 3]) // true
+// isHomogenous(['a', 'b', 'c']) // true
+// isHomogenous([[2], 'Xavier']) // false
+// isHomogenous([[2], {colour: 'blue'}]) // false
+// isHomogenous([1, '2', 3]) // false
