408f82726e7641bef8b9aa56be232d56bcc7b3eb
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-04 10:28:12 -0800

    created the occurencesOf functiond

---
+++ C1OccurencesOf.js
@@ -0,0 +1,10 @@
+const occurencesOf = (a,b) => {
+    let count = 0;
+    for (let i in arr) {
+        if (b[i] === a) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log('o', 'hellow how are you')
