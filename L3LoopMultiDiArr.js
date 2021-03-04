9e085123a63c7817b4628eaf97cd6cefcf27c479
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-04 14:56:06 -0800

    added comments to L3LoopMultiDiArr.js

--- L3LoopMultiDiArr.js
+++ L3LoopMultiDiArr.js
@@ -1,8 +1,14 @@
 const printMulti = (multiArr) => {
     let outputStr = '\n';
+    // This will be the final output all in one string.
+    // \n is not needed however makes it easier for me
+    // to see output.
     for (let arr of multiArr) {
+        // arr is the inner array of multiArr
         for (let val of arr) {
             outputStr += `${val}\n`;
+            // taking each value of arr as val and adding them
+            // as a string to outputStr.
         }
     }
     return outputStr;
