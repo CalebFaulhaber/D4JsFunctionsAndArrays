1709ac1bc8ec83af7173fb2639f15a7c8098d3d1
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-04 15:28:05 -0800

    created 7th lab file

---
+++ L7IsEven.js
@@ -0,0 +1,9 @@
+const isEven = (num) => {
+
+};
+
+console.log(isEven(5));
+
+// isEven
+
+// Write a function, isEven, that takes a number as argument. It returns true if the number is even otherwise it returns false. You are not allowed to use the modulus operator (%) nor the division operator (/).
