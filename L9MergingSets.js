647e3e9e2f152dfce6a0182c74959521ab8cab34
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-04 20:07:21 -0800

    created 9th lab file

---
+++ L9MergingSets.js
@@ -0,0 +1,12 @@
+
+
+
+// Merging Sets
+
+// Write a function, mergeSet, that merges two arrays and removes all duplicate elements.
+
+// Example:
+// let array1 = [1, 2, 3];
+// let array2 = [3, 4, 5];
+// console.log(mergeSet(array1, array2));
+// // [1, 2, 3, 4, 5]
