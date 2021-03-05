7dfd76cdfbe3bf271b492688ba0b6bce9c6b7bca
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-05 09:45:23 -0800

    Finished mergeSets function for 9th lab and left comments. Function works.

--- L9MergingSets.js
+++ L9MergingSets.js
@@ -1,4 +1,23 @@
+const mergeSet = (arr1, arr2) => {
+    let zipArr = [];
+    for (let num of arr1) {
+        if (!zipArr.includes(num) && !isNaN(num) && num !== null) {
+            zipArr.push(num);
+            // If zipArr does not contain num, num is a number
+            // and num doesn't equal null then it will push
+            // num to zipArr.
+        }
+    }
+    for (let num of arr2) {
+        if (!zipArr.includes(num) && !isNaN(num) && num !== null) {
+            zipArr.push(num);
+            // Same as lines 3-10 but for arr2.
+        }
+    }
+    return zipArr;
+};
 
+console.log(mergeSet([1,2,3], [3,4,5]));
 
 
 // Merging Sets
